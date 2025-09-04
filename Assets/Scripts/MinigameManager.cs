b5dc18a3b4b562dad49944027f8cb52963e87902
caprisunsly <aidanthebeech@gmail.com>
2025-09-04 02:33:19 +0100

    random microgame selection

--- Assets/Scripts/MinigameManager.cs
+++ Assets/Scripts/MinigameManager.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MinigameManager : MonoBehaviour
 {
+    [SerializeField] List<SO_Microgame> m_MicrogameMasterList = new();
+    List<SO_Microgame> m_MicrogameCurrentList = new();
+
     //radio stations
     public static event Action OnMinigameStarted;
     public static event Action OnMinigameEnded;
 
-    //choose a game & store a buffer of selected games
+    //choose a game from current list. if empty, pull from master list
+    //pick a random index from current list
+
     //load the game scene
     //invoke OnMinigameStarted
     //wait until the game is finished (time runs out)
@@ -18,6 +24,10 @@ public class MinigameManager : MonoBehaviour
 
     IEnumerator MinigameLoop()
     {
+        if (m_MicrogameCurrentList.Count <= 0) m_MicrogameCurrentList = new List<SO_Microgame>(m_MicrogameMasterList);
+        int index = UnityEngine.Random.Range(0, m_MicrogameCurrentList.Count - 1);
+        SO_Microgame selectedMicrogame = m_MicrogameCurrentList[index];
+        //load scene
         yield return null;
     }
 }
